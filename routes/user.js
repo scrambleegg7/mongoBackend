f10cbfb29bb5e93b9feb57dca6434057a621b70a
scrambleegg7 <scrambleegg7@gmail.com>
2020-05-18 23:26:42 +0900

    suppress users routing for live settings.

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ const router = express.Router();
 router.put("/user/follow", requireSignin, addFollowing, addFollower);
 router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
 
-router.get("/users", allUsers);
+//router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
 //router.put("/user/:userId", requireSignin, hasAuthorization, updateUser);
 router.put("/user/:userId", requireSignin,  updateUser);
